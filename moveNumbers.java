e7339784475c3aaafe7591ae23249af404901cce
Divya <ayvidrajpoot21@gmail.com>
2022-09-08 13:30:05 +0530

    added a space

--- moveNumbers.java
+++ moveNumbers.java
@@ -3,6 +3,7 @@
 // An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
 
 // https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
+
 import java.util.*;
 
 public class moveNumbers {
